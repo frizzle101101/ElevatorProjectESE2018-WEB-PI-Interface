4284124007880316a17bbf5036ca3aa9b62786e7
frizzle101101 <aaronfritz96@gmail.com>
2018-07-23 17:45:35 -0400

    -fixing elv_control buttons

---
+++ reqFloor.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+  // get the q parameter from URL
+  $q = $_REQUEST["q"];
+
+
+  if(isset($_SESSION['username']))
+  {
+    // lookup all hints from array if $q is different from ""
+    if ($q !== "") {
+        $PDOMySQL = new PDOMySQL();
+        $PDOMySQL->requestFloor((int)$q);
+    }
+
+  }
+
+
+
+?>
--- request_access.php
+++ request_access.php
@@ -1,16 +1,35 @@
 <?php
-  // get the q parameter from URL
-  $q = $_REQUEST["q"];
-
+  require 'menu.php';
+  require 'PDOMySQL.php';
+  $PDOMySQL = new PDOMySQL();
+  if(!empty($_POST))
+  {
+    foreach ($_POST['interest'] as $key => $value) {
+      $interest = $interest. $value;
+    }
+    $PDOMySQL->insertUser(
+      $_POST['username'],
+      $_POST['password'],
+      $_POST['first_name'],
+      $_POST['last_name'],
+      $_POST['email'],
+      $_POST['website'],
+      $_POST['birth_date'],
+      $_POST['user_type'],
+      $interest
+    );
+      $_SESSION['username'] = $_POST['username'];
+  }
 
   if(isset($_SESSION['username']))
   {
-    // lookup all hints from array if $q is different from ""
-    if ($q !== "") {
-        $PDOMySQL = new PDOMySQL();
-        $PDOMySQL->requestFloor((int)$q);
-    }
-
+    echo "You are loged in, will redirect you to account page";
+    header("Location: account.php");
+    die();
+  }
+  else
+  {
+    require 'html/request_access.html';
   }
 
 
