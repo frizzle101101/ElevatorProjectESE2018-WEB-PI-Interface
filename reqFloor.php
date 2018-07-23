6c78abf280c07b8c0676c68b50e937b626da9e82
frizzle101101 <aaronfritz96@gmail.com>
2018-07-23 19:29:42 -0400

    -removing session from reqFloor.php

--- reqFloor.php
+++ reqFloor.php
@@ -1,17 +1,10 @@
 <?php
-  session_start();
   // get the q parameter from URL
   $q = $_REQUEST["q"];
-
-
-  if(isset($_SESSION['username']))
-  {
-    // lookup all hints from array if $q is different from ""
-    if ($q !== "") {
-        $PDOMySQL = new PDOMySQL();
-        $PDOMySQL->requestFloor((int)$q);
-    }
-
+  // lookup all hints from array if $q is different from ""
+  if ($q !== "") {
+      $PDOMySQL = new PDOMySQL();
+      $PDOMySQL->requestFloor((int)$q);
   }
 
 
