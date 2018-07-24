9d5c2db534cdf80a14c2bb2249ac4717247b899e
frizzle101101 <aaronfritz96@gmail.com>
2018-07-23 20:06:43 -0400

    -removing unused lines

--- reqFloor.php
+++ reqFloor.php
@@ -1,10 +1,10 @@
 <?php
   require 'PDOMySQL.php';
+  $PDOMySQL = new PDOMySQL();
   // get the q parameter from URL
   $q = $_REQUEST["q"];
   // lookup all hints from array if $q is different from ""
   if ($q !== "") {
-      $PDOMySQL = new PDOMySQL();
       $PDOMySQL->requestFloor((int)$q);
   }
 
