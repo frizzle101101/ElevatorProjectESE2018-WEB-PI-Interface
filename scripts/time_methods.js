e8f5fe77301641400b106bba791b54db16e1a076
awbonnell <39132443+awbonnell@users.noreply.github.com>
2018-08-02 12:12:58 -0400

    Removed Commented test code

--- scripts/time_methods.js
+++ scripts/time_methods.js
@@ -1,14 +1,4 @@
 
-/*
-var currTime = new Date();
-var months = ['January','February','March','April','May','June','July','August','September','October','November', 'December'];
-var dateID = document.getElementById('cDate');
-var timeID = document.getElementById('cTime');
-
-dateID.innerHTML = months[currTime.getMonth()] + ' ' + currTime.getDate() + ' ' + currTime.getFullYear();
-timeID.innerHTML = ' ' + currTime.getHours() + ':' + currTime.getMinutes() + ':' + currTime.getSeconds();
-
-*/
 
 function showTime(){
 	var currTime = new Date();
@@ -21,5 +11,5 @@ function showTime(){
 
 
 }
-//document.getElementById('cDate').innerHTML = "TEST";
+
 setInterval(showTime, 1000);//call again in 1 second
